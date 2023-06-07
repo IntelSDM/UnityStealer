c1804d96ff4855ce10e2b5d513ceb52feb920da8
IntelSDM <GitBackup@shibemaster.me>
2023-06-07 22:14:25 +0100

    Getting Game

--- Payload/GameInfo.cs
+++ Payload/GameInfo.cs
@@ -1,22 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Reflection;
+using UnityEngine;
 namespace Payload
 {
     class GameInfo
     {
         public GameInfo()
         {
-            
+            SetGame();
         }
-        void GuessGame()
+        void SetGame()
         {
-            
+            // could call getfilename and pass null but this just seems acceptable
+
+            Name = System.Diagnostics.Process.GetCurrentProcess().ProcessName;
+            ProcessTarget = Name + ".exe";
+
+            File.WriteAllText("test.txt", Name);
+            File.WriteAllText("test2.txt", ProcessTarget);
         }
-        string Name;
-        string ProcessTarget;
+        private string Name;
+        private string ProcessTarget;
     }
 }
--- Payload/Load.cs
+++ Payload/Load.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.IO;
 namespace Payload
 {
     public class Load
@@ -11,5 +11,13 @@ namespace Payload
         // drop in game directory
         // change game directory to 7days/7days
         // keep original which opens and then closes and opens the next 7 days
+       
+        public static void Init()
+        {
+            File.WriteAllText("test.txt","");
+            GameInfo gameinfo = new GameInfo();
+            
+
+        }
     }
 }
